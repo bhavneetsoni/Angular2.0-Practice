158cc2682d623ffbd5e3e968ba5bfd149abd6946
Bhavneet Soni <bhavneet.ui@gmail.com>
2016-10-02 18:13:44 -0500

    Angular 2 Practice app

---
+++ app/authors.component.ts
@@ -0,0 +1,17 @@
+﻿import { Component } from 'angular2/core';
+
+@Component({
+    selector: 'authors',
+    template: `
+    <h2>Authors</h2>
+{{title}}
+<ul>
+<li *ngFor = '#author of authors'>{{author}}</li>
+</ul>
+
+`
+})
+
+export class AuthorsComponent{
+    authors;
+}
